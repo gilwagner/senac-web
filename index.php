f3ce3d5ce2c7b4903548f69ff110e08531aa4909
gil wagner <gilwagnersousachaves@gmail.com>
2015-11-03 16:50:40 -0200

    realizado a soma

--- index.php
+++ index.php
@@ -4,7 +4,22 @@ require 'vendor/autoload.php';
 $app = new \Slim\Slim();
 
 $app->get('/hello/:name', function ($name) {
-    echo "Hello, " . $name;
+    $inteiro1 = 42;
+    $inteiro2 = 79;
+    $texto = "ola, "; 
+    $fracao = 2.3;
+    
+    $soma = $inteiro1 + $inteiro2;
+    $divisao = $inteiro1 / $fracao;
+    $multi = $inteiro2 * $fracao;
+    $sub = $inteiro1 - $inteiro2;
+    
+    $olaMundo = $texto . $name;
+    
+    echo "<p>Soma: ". $soma . "</p>";
+    echo "<p>Divisao: ". $divisao . "</p>";
+    echo "<p>Multiplicao: ". $multi . "</p>";
+    echo "<p>". $olaMundo . "</p>";
 });
 
-$app->run(); 
+$app->run();
